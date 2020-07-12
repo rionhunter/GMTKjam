5953ce29ff364e3635f653901b47349acc5106a0
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-07-12 12:51:59 -0700

    fixed issue

--- event_hub.gd
+++ event_hub.gd
@@ -9,3 +9,11 @@ signal queue_check
 # From player
 signal new_thought
 signal new_action
+signal new_destination
+signal in_house
+signal outside
+signal animate
+
+# From character
+signal reached_destination
+signal animation_done
