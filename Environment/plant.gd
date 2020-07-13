3112dc17f19790f9d21343b256a5d5bc7e911224
rion <rionbrodie@gmail.com>
2020-07-14 01:01:33 +1000

    additional files

---
+++ Environment/plant.gd
@@ -0,0 +1,13 @@
+extends Node
+
+enum plantStates {soil, sowed, sapling, growing, grown, fruiting}
+var state = plantStates.soil
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
